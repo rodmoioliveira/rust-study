ad0fd5494818c91d002a21f22d5a123f612b9979
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2020-09-30 00:26:40 -0300

    add match_control

---
+++ match_control/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// The match Control Flow Operator
+// https://doc.rust-lang.org/book/ch06-02-match.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
