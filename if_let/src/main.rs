9169ed07ec21e9a1189e26b88f5fb388f7921d7d
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2020-10-05 00:46:24 -0300

    Start if_let crate

---
+++ if_let/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Concise Control Flow with if let
+// https://doc.rust-lang.org/book/ch06-03-if-let.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
