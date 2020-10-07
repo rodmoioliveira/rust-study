eda3c65ac12f3d8735819c7a28b4534c146725fd
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-10-06 23:38:54 -0300

    update

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -50,6 +50,7 @@ pub mod front_of_house {
         pub fn serve_order() {
             println!("serve_order");
             crate::front_of_house::serving::hide();
+            super::serving::hide();
         }
 
         pub fn take_payment() {
@@ -196,3 +197,89 @@ pub mod front_of_house {
 // Starting Relative Paths with super
 // https://doc.rust-lang.org/book/ch07-03-paths-for-referring-to-an-item-in-the-module-tree.html#starting-relative-paths-with-super
 // =======================================
+
+// We can also construct relative paths that begin in the parent module by using super at the start
+// of the path. This is like starting a filesystem path with the .. syntax. Why would we want to do
+// this?
+
+// Consider the code in Listing 7-8 that models the situation in which a chef fixes an incorrect
+// order and personally brings it out to the customer. The function fix_incorrect_order calls the
+// function serve_order by specifying the path to serve_order starting with super:
+
+// fn serve_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::serve_order();
+//     }
+
+//     fn cook_order() {}
+// }
+
+// The fix_incorrect_order function is in the back_of_house module, so we can use super to go to
+// the parent module of back_of_house, which in this case is crate, the root. From there, we look
+// for serve_order and find it. Success! We think the back_of_house module and the serve_order
+// function are likely to stay in the same relationship to each other and get moved together should
+// we decide to reorganize the crate’s module tree. Therefore, we used super so we’ll have fewer
+// places to update code in the future if this code gets moved to a different module.
+
+// =======================================
+// Making Structs and Enums Public
+// =======================================
+// We can also use pub to designate structs and enums as public, but there are a few extra details.
+// If we use pub before a struct definition, we make the struct public, but the struct’s fields
+// will still be private. We can make each field public or not on a case-by-case basis. In Listing
+// 7-9, we’ve defined a public back_of_house::Breakfast struct with a public toast field but a
+// private seasonal_fruit field. This models the case in a restaurant where the customer can pick
+// the type of bread that comes with a meal, but the chef decides which fruit accompanies the meal
+// based on what’s in season and in stock. The available fruit changes quickly, so customers can’t
+// choose the fruit or even see which fruit they’ll get.
+
+pub mod back_of_house {
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // Because the toast field in the back_of_house::Breakfast struct is public, in eat_at_restaurant
+    // we can write and read to the toast field using dot notation. Notice that we can’t use the
+    // seasonal_fruit field in eat_at_restaurant because seasonal_fruit is private. Try uncommenting
+    // the line modifying the seasonal_fruit field value to see what error you get!
+
+    // Also, note that because back_of_house::Breakfast has a private field, the struct needs to
+    // provide a public associated function that constructs an instance of Breakfast (we’ve named it
+    // summer here). If Breakfast didn’t have such a function, we couldn’t create an instance of
+    // Breakfast in eat_at_restaurant because we couldn’t set the value of the private seasonal_fruit
+    // field in eat_at_restaurant.
+
+    // In contrast, if we make an enum public, all of its variants are then public. We only need the
+    // pub before the enum keyword, as shown in Listing 7-10.
+
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    // Because we made the Appetizer enum public, we can use the Soup and Salad variants in
+    // eat_at_restaurant. Enums aren’t very useful unless their variants are public; it would be
+    // annoying to have to annotate all enum variants with pub in every case, so the default for
+    // enum variants is to be public. Structs are often useful without their fields being public,
+    // so struct fields follow the general rule of everything being private by default unless
+    // annotated with pub.
+
+    // There’s one more situation involving pub that we haven’t covered, and that is our last module
+    // system feature: the use keyword. We’ll cover use by itself first, and then we’ll show how to
+    // combine pub and use.
+}
--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -1,4 +1,6 @@
+use restaurant::back_of_house;
 use restaurant::front_of_house;
+// use restaurant::*;
 
 fn main() {
     front_of_house::hosting::add_to_waitlist();
@@ -6,4 +8,17 @@ fn main() {
     front_of_house::serving::take_order();
     front_of_house::serving::take_payment();
     front_of_house::serving::serve_order();
+
+    let mut breakfast = back_of_house::Breakfast::summer("rye");
+    breakfast.toast = String::from("wheat");
+
+    // // private, will break the code...
+    // breakfast.seasonal_fruit = String::from("apple");
+
+    let ap1 = back_of_house::Appetizer::Soup;
+    let ap2 = back_of_house::Appetizer::Salad;
+
+    println!("{:?}", breakfast);
+    println!("{:?}", ap1);
+    println!("{:?}", ap2);
 }
