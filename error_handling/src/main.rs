1700a3e687c46a663a3186ba4c231926f56bc6de
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-10-19 12:55:39 -0300

    add error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Error Handling
+// https://doc.rust-lang.org/book/ch09-00-error-handling.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
