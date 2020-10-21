651a8f22eeaf389d547a694f5ceb0e732638b24c
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-10-21 12:49:14 -0300

    add generic_types

---
+++ generic_types/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Generic Types, Traits, and Lifetimes
+// https://doc.rust-lang.org/book/ch10-00-generics.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
