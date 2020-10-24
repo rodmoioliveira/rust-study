cdcedea5080176b5baaf19634d8a62818d1a02ae
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-10-24 15:17:21 -0300

    update

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Validating References with Lifetimes
+// https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
