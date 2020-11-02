a8cf62d391c576164f7edd715e36596f0afe46b2
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-02 19:01:46 -0300

    update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -230,12 +230,12 @@ fn main() {
     // the config instance in the fields named for their purpose.
 
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
