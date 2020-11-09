fc76379b286ae96e54adbf7b89f4e35267592042
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-09 20:13:32 -0300

    update

---
+++ dereference/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Treating Smart Pointers Like Regular References with the Deref Trait
+// https://doc.rust-lang.org/book/ch15-02-deref.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
