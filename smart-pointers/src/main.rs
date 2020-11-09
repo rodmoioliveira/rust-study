40bee12b5fb3d70607132d3e7d1d09c51302fd52
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-09 20:11:49 -0300

    update

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -146,14 +146,20 @@ fn main() {
     // example. We could have implemented it using generics, as we discussed in Chapter 10, to
     // define a cons list type that could store values of any type.
 
+    #[derive(Debug)]
     enum List {
-        Cons(i32, List),
+        Cons(i32, Box<List>),
         Nil,
     }
 
     // Using the List type to store the list 1, 2, 3 would look like the code in Listing 15-3:
 
-    let list = List::Cons(1, List::Cons(2, List::Cons(3, List::Nil)));
+    let list = List::Cons(
+        1,
+        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    );
+
+    println!("{:?}", list);
 
     // The first Cons value holds 1 and another List value. This List value is another Cons value
     // that holds 2 and another List value. This List value is one more Cons value that holds 3 and
@@ -197,4 +203,42 @@ fn main() {
     // Using Box<T> to Get a Recursive Type with a Known Size
     // https://doc.rust-lang.org/book/ch15-01-box.html#using-boxt-to-get-a-recursive-type-with-a-known-size
     // =======================================
+    // Rust can’t figure out how much space to allocate for recursively defined types, so the
+    // compiler gives the error in Listing 15-4. But the error does include this helpful
+    // suggestion:
+
+    // = help: insert indirection (e.g., a `Box`, `Rc`, or `&`) at some point to make `List` representable
+
+    // In this suggestion, “indirection” means that instead of storing a value directly, we’ll change
+    // the data structure to store the value indirectly by storing a pointer to the value instead.
+
+    // Because a Box<T> is a pointer, Rust always knows how much space a Box<T> needs: a pointer’s
+    // size doesn’t change based on the amount of data it’s pointing to. This means we can put a
+    // Box<T> inside the Cons variant instead of another List value directly. The Box<T> will point
+    // to the next List value that will be on the heap rather than inside the Cons variant.
+    // Conceptually, we still have a list, created with lists “holding” other lists, but this
+    // implementation is now more like placing the items next to one another rather than inside one
+    // another.
+
+    // We can change the definition of the List enum in Listing 15-2 and the usage of the List in
+    // Listing 15-3 to the code in Listing 15-5, which will compile:
+
+    // The Cons variant will need the size of an i32 plus the space to store the box’s pointer
+    // data. The Nil variant stores no values, so it needs less space than the Cons variant. We now
+    // know that any List value will take up the size of an i32 plus the size of a box’s pointer
+    // data. By using a box, we’ve broken the infinite, recursive chain, so the compiler can figure
+    // out the size it needs to store a List value.
+
+    // Boxes provide only the indirection and heap allocation; they don’t have any other special
+    // capabilities, like those we’ll see with the other smart pointer types. They also don’t have
+    // any performance overhead that these special capabilities incur, so they can be useful in
+    // cases like the cons list where the indirection is the only feature we need. We’ll look at
+    // more use cases for boxes in Chapter 17, too.
+
+    // The Box<T> type is a smart pointer because it implements the Deref trait, which allows Box<T>
+    // values to be treated like references. When a Box<T> value goes out of scope, the heap data that
+    // the box is pointing to is cleaned up as well because of the Drop trait implementation. Let’s
+    // explore these two traits in more detail. These two traits will be even more important to the
+    // functionality provided by the other smart pointer types we’ll discuss in the rest of this
+    // chapter.
 }
