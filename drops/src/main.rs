27a8bc6b18bbdaf5c0d6dd2ce1753dd93a6783f4
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-10 21:26:54 -0300

    update

---
+++ drops/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Running Code on Cleanup with the Drop Trait
+// https://doc.rust-lang.org/book/ch15-03-drop.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
