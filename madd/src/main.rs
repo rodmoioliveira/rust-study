3526102bbae7e8ebdc227662c874d46ff6f1c45e
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-13 15:52:09 -0300

    adiciona binário mprd

--- madd/src/main.rs
+++ madd/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
 fn main() {
-    let sum: i128 = env::args().filter_map(|s| s.parse::<i128>().ok()).product();
+    let sum: i128 = env::args().filter_map(|s| s.parse::<i128>().ok()).sum();
     println!("{}", sum);
 }
