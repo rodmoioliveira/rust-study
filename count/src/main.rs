74b40e86c64fbe6fe7af32d3ebb1b1ab8d573790
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-11-13 22:41:09 -0300

    update

--- count/src/main.rs
+++ count/src/main.rs
@@ -1,5 +1,30 @@
 use regex::Regex;
-use std::{collections::HashMap, env, error::Error, fs, process};
+use std::{
+    collections::HashMap,
+    env,
+    error::Error,
+    fs::{self, DirEntry},
+    io,
+    path::Path,
+    process,
+};
+
+fn get_files(dir: String, files: &mut Vec<String>) -> io::Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            if let Err(e) = get_files(path.to_str().unwrap().to_string(), files) {
+                eprintln!("Application error: {}", e);
+                process::exit(1);
+            }
+        } else {
+            files.push(path.to_str().unwrap().to_string());
+        }
+    }
+
+    Ok(())
+}
 
 fn count_words(
     filename: String,
@@ -15,24 +40,31 @@ fn count_words(
         *count += 1;
     }
 
-    return Ok(());
+    Ok(())
 }
 
 fn main() {
-    let mut table: HashMap<String, u64> = HashMap::new();
-    let filename: String = env::args().nth(1).unwrap();
-    let re = Regex::new(r"[!-/:-@\[\]`{}-~]").unwrap();
+    // let mut table: HashMap<String, u64> = HashMap::new();
+    // let filename: String = env::args().nth(1).unwrap();
+    // let re = Regex::new(r"[!-/:-@\[\]`{}-~]").unwrap();
+    let mut files: Vec<String> = Vec::new();
 
-    // TODO: Recursive call de todos os arquivos dentro do diretório
-    if let Err(e) = count_words(filename, &mut table, &re) {
+    // // TODO: Recursive call de todos os arquivos dentro do diretório
+    // if let Err(e) = count_words(filename, &mut table, &re) {
+    //     eprintln!("Application error: {}", e);
+    //     process::exit(1);
+    // }
+
+    // let mut result: Vec<(String, u64)> = table.into_iter().collect();
+    // result.sort_by(|a, b| b.1.cmp(&a.1));
+
+    // for (name, value) in result {
+    //     println!("{}: {}", name, value);
+    // }
+
+    if let Err(e) = get_files(".".to_string(), &mut files) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
-
-    let mut result: Vec<(String, u64)> = table.into_iter().collect();
-    result.sort_by(|a, b| b.1.cmp(&a.1));
-
-    for (name, value) in result {
-        println!("{}: {}", name, value);
-    }
+    println!("{:?}", files);
 }
