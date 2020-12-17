2a65d6d10ce44c95b4487dfdfaced3c096d794fc
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-12-16 23:34:21 -0300

    chore: adiciona concurrency

---
+++ concurrency/src/main.rs
@@ -0,0 +1,6 @@
+// =======================================
+// https://doc.rust-lang.org/book/ch16-00-concurrency.html#fearless-concurrency
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
