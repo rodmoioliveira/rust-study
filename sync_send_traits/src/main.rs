52ef9506dc1eb72049a629b914cef72448dca586
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-12-20 22:40:42 -0300

    update

---
+++ sync_send_traits/src/main.rs
@@ -0,0 +1,7 @@
+// =======================================
+// Extensible Concurrency with the Sync and Send Traits
+// https://doc.rust-lang.org/book/ch16-04-extensible-concurrency-sync-and-send.html
+// =======================================
+fn main() {
+    println!("Hello, world!");
+}
