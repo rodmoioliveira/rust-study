de2f637e05dcd54762cd08ae4127467d3214fb90
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-12-20 21:08:06 -0300

    update

---
+++ channels/src/main.rs
@@ -0,0 +1,8 @@
+// =======================================
+// Using Message Passing to Transfer Data Between Threads
+// https://doc.rust-lang.org/book/ch16-02-message-passing.html
+// =======================================
+
+fn main() {
+    println!("Hello, world!");
+}
