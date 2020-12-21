9f610d682b00b14d73a7fa1c393a705c8a1c5a78
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-12-21 18:52:41 -0300

    update

---
+++ oo_features/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // =======================================
+    // Object Oriented Programming Features of Rust
+    // https://doc.rust-lang.org/book/ch17-00-oop.html
+    // =======================================
+    println!("Hello, world!");
+}
